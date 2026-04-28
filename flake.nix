ffd4a73e27bef5676e2c99b17a83496946603351
simotro <mail@simonetromboni.it>
2026-04-28 16:08:38 +0200

    Corretto flake.nix

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
           home-manager.sharedModules = [ plasma-manager.homeManagerModules.plasma-manager ];
-          home-manager.users.tuoutente = import ./modules/home.nix;
+          home-manager.users.simone = import ./modules/home.nix;
         }
       ];
     };

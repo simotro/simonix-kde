1f489cae44dee99a7c0c73d1eccb667d526ddd5f
simotro <mail@simonetromboni.it>
2026-04-28 17:23:41 +0200

    Correzioni...

--- modules/home.nix
+++ modules/home.nix
@@ -1,12 +1,20 @@
-{ pkgs, ... }: {
-  home.stateVersion = "26.05";
-  };
+{ pkgs, ... }:
 
+{
+  home.username = "simone";
+  home.homeDirectory = "/home/simone";
+
+  home.stateVersion = "26.05";
+
+  # Qui metterai i tuoi pacchetti utente
   home.packages = with pkgs; [
     firefox
     insync
     megasync
     proton-pass
-    vscodium
+    #vscodium
   ];
+
+  # Abilita Home Manager a gestire se stesso
+  programs.home-manager.enable = true;
 }
